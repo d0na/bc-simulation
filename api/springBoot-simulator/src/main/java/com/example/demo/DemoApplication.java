5ee03f908de45eea9dd72324b4468050c661b716
Francesco <francesco.donini@iit.cnr.it>
2025-04-14 08:29:17 +0200

    file cleaning

--- api/springBoot-simulator/src/main/java/com/example/demo/DemoApplication.java
+++ api/springBoot-simulator/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,5 @@
 package com.example.demo;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
