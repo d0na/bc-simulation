eeb19b9fa5b2baed01bd379c1c0bb36b8d48da8c
fancesco <francesco.donini@iit.cnr.it>
2025-04-29 16:05:57 +0200

    api: fix and review plotters to use with gnuplot

--- api/springBoot-simulator/doplotAll1Aggr2weeksFINAL.sh
+++ api/springBoot-simulator/doplotAll1Aggr2weeksFINAL.sh
@@ -35,90 +35,95 @@ set key font 'Times-New-Roman,18 \n";
 
 #set yrange [0:6800000]\n\
 
-#fileIn1=./output/86400a1_20250428173847
-#fileOut="simGraph1Variance"
-#echo -e "set output \"$fileOut.$outf\"\n\
-#$same\n\
-#set title 'Single peak'\n\
-#plot   '$fileIn1.tsv' using 1:(\$2-\$3):(\$2+\$3) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn1.tsv' using 1:2 lc \"skyblue\" pt 9 title 'mean', '$fileIn1.tsv' using 1:2 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\"\n"  | gnuplot -persist
-#
-#fileIn2=simResultsTest6Scaledt1209600a60
-#fileOut="simGraph2Variance"
-#echo -e "set output \"$fileOut.$outf\"\n\
-#$same\n\
-#set title 'Boom and bust'\n\
-#plot   '$fileIn2.tsv' using 1:(\$2-\$3):(\$2+\$3) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn2.tsv' using 1:2 lc \"skyblue\" pt 9 title 'mean', '$fileIn2.tsv' using 1:2 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\"\n"  | gnuplot -persist
-#
-#fileOut="simGraph1Creators"
-#echo -e "set output \"$fileOut.$outf\"\n\
-#$same\n\
-#set ylabel 'number of creators'\n\
-#set title 'Single peak'\n\
-#plot   '$fileIn1.tsv' using 1:(\$17-\$18):(\$17+\$18) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn1.tsv' using 1:17 lc \"skyblue\" pt 9 title 'mean', '$fileIn1.tsv' using 1:17 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\", '$fileIn1.tsv' using 1:15 with lines title 'max' smooth bezier lw 3 lc \"red\", '$fileIn1.tsv' using 1:16 with lines title 'min' smooth bezier lw 3 lc \"green\"\n"  | gnuplot -persist
-#
-#fileOut="simGraph1Assets"
-#echo -e "set output \"$fileOut.$outf\"\n\
-#$same\n\
-#set ylabel 'number of assets'\n\
-#set title 'Single peak'\n\
-#plot   '$fileIn1.tsv' using 1:(\$22-\$23):(\$22+\$23) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn1.tsv' using 1:22 lc \"skyblue\" pt 9 title 'mean', '$fileIn1.tsv' using 1:22 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\", '$fileIn1.tsv' using 1:20 with lines title 'max' smooth bezier lw 3 lc \"red\", '$fileIn1.tsv' using 1:21 with lines title 'min' smooth bezier lw 3 lc \"green\"\n"  | gnuplot -persist
-#
-#fileOut="simGraph2Creators"
-#echo -e "set output \"$fileOut.$outf\"\n\
-#$same\n\
-#set ylabel 'number of creators'\n\
-#set title 'Boom and bust'\n\
-#plot   '$fileIn2.tsv' using 1:(\$17-\$18):(\$17+\$18) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn2.tsv' using 1:17 lc \"skyblue\" pt 9 title 'mean', '$fileIn2.tsv' using 1:17 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\", '$fileIn2.tsv' using 1:15 with lines title 'max' smooth bezier lw 3 lc \"red\", '$fileIn2.tsv' using 1:16 with lines title 'min' smooth bezier lw 3 lc \"green\"\n"  | gnuplot -persist
-#
-#fileOut="simGraph2Assets"
-#echo -e "set output \"$fileOut.$outf\"\n\
-#$same\n\
-#set ylabel 'number of assets'\n\
-#set title 'Boom and bust'\n\
-#plot   '$fileIn2.tsv' using 1:(\$22-\$23):(\$22+\$23) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn2.tsv' using 1:22 lc \"skyblue\" pt 9 title 'mean', '$fileIn2.tsv' using 1:22 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\", '$fileIn2.tsv' using 1:20 with lines title 'max' smooth bezier lw 3 lc \"red\", '$fileIn2.tsv' using 1:21 with lines title 'min' smooth bezier lw 3 lc \"green\"\n"  | gnuplot -persist
-#
-#
-#
-#
-#
-#
-#
-##fileOut="simGraph1Operations"
-##size="600,600"
-##echo -e "set output \"$fileOut.$outf\"\n\
-##$same\n\
-##set ylabel 'gas'\n\
-##set title 'Single peak'\n\
-##set yrange [0:70100000]\n\
-##set terminal $outff $font size $size\n\
-##plot   '$fileIn1.tsv' using 1:2 with lines title 'Total' smooth bezier lw 3 lc \"medium-blue\", '$fileIn1.tsv' using 1:12 with filledcurves smooth bezie title 'transferFrom' lc \"light-red\",  '$fileIn1.tsv' using 1:10 with filledcurves smooth bezie title 'setColor' lc \"goldenrod\", '$fileIn1.tsv' using 1:8 with filledcurves smooth bezie title 'hPolicyUpdate' lc \"dark-yellow\", '$fileIn1.tsv' using 1:6 with filledcurves smooth bezie title 'newAsset' lc \"dark-orange\", '$fileIn1.tsv' using 1:4 with filledcurves smooth bezie title 'newCreator' lc \"green\"\n"  | gnuplot -persist
-#
-#
+fileIn1=simResultsTest5Scaledt1209600a60
+fileOut="simGraph1Variance"
+echo -e "set output \"$fileOut.$outf\"\n\
+$same\n\
+set title 'Single peak'\n\
+plot   '$fileIn1.tsv' using 1:(\$2-\$3):(\$2+\$3) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn1.tsv' using 1:2 lc \"skyblue\" pt 9 title 'mean', '$fileIn1.tsv' using 1:2 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\"\n"  | gnuplot -persist
+
+fileIn2=simResultsTest6Scaledt1209600a60
+fileOut="simGraph2Variance"
+echo -e "set output \"$fileOut.$outf\"\n\
+$same\n\
+set title 'Boom and bust'\n\
+plot   '$fileIn2.tsv' using 1:(\$2-\$3):(\$2+\$3) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn2.tsv' using 1:2 lc \"skyblue\" pt 9 title 'mean', '$fileIn2.tsv' using 1:2 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\"\n"  | gnuplot -persist
+
+fileOut="simGraph1Creators"
+echo -e "set output \"$fileOut.$outf\"\n\
+$same\n\
+set ylabel 'number of creators'\n\
+set title 'Single peak'\n\
+plot   '$fileIn1.tsv' using 1:(\$17-\$18):(\$17+\$18) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn1.tsv' using 1:17 lc \"skyblue\" pt 9 title 'mean', '$fileIn1.tsv' using 1:17 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\", '$fileIn1.tsv' using 1:15 with lines title 'max' smooth bezier lw 3 lc \"red\", '$fileIn1.tsv' using 1:16 with lines title 'min' smooth bezier lw 3 lc \"green\"\n"  | gnuplot -persist
+
+fileOut="simGraph1Assets"
+echo -e "set output \"$fileOut.$outf\"\n\
+$same\n\
+set ylabel 'number of assets'\n\
+set title 'Single peak'\n\
+plot   '$fileIn1.tsv' using 1:(\$22-\$23):(\$22+\$23) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn1.tsv' using 1:22 lc \"skyblue\" pt 9 title 'mean', '$fileIn1.tsv' using 1:22 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\", '$fileIn1.tsv' using 1:20 with lines title 'max' smooth bezier lw 3 lc \"red\", '$fileIn1.tsv' using 1:21 with lines title 'min' smooth bezier lw 3 lc \"green\"\n"  | gnuplot -persist
+
+fileOut="simGraph2Creators"
+echo -e "set output \"$fileOut.$outf\"\n\
+$same\n\
+set ylabel 'number of creators'\n\
+set title 'Boom and bust'\n\
+plot   '$fileIn2.tsv' using 1:(\$17-\$18):(\$17+\$18) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn2.tsv' using 1:17 lc \"skyblue\" pt 9 title 'mean', '$fileIn2.tsv' using 1:17 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\", '$fileIn2.tsv' using 1:15 with lines title 'max' smooth bezier lw 3 lc \"red\", '$fileIn2.tsv' using 1:16 with lines title 'min' smooth bezier lw 3 lc \"green\"\n"  | gnuplot -persist
+
+fileOut="simGraph2Assets"
+echo -e "set output \"$fileOut.$outf\"\n\
+$same\n\
+set ylabel 'number of assets'\n\
+set title 'Boom and bust'\n\
+plot   '$fileIn2.tsv' using 1:(\$22-\$23):(\$22+\$23) with filledcurves lc \"skyblue\" fs transparent solid 0.35 title 'std', '$fileIn2.tsv' using 1:22 lc \"skyblue\" pt 9 title 'mean', '$fileIn2.tsv' using 1:22 with lines title 'bezier' smooth bezier lw 3 lc \"medium-blue\", '$fileIn2.tsv' using 1:20 with lines title 'max' smooth bezier lw 3 lc \"red\", '$fileIn2.tsv' using 1:21 with lines title 'min' smooth bezier lw 3 lc \"green\"\n"  | gnuplot -persist
+
+
+
+
+
+
+
 #fileOut="simGraph1Operations"
 #size="600,600"
 #echo -e "set output \"$fileOut.$outf\"\n\
 #$same\n\
 #set ylabel 'gas'\n\
 #set title 'Single peak'\n\
-#set yrange [0:90000000]\n\
+#set yrange [0:70100000]\n\
 #set terminal $outff $font size $size\n\
-#plot   '$fileIn1.tsv' using 1:2 with lines title 'Total' smooth bezier lw 3 lc \"medium-blue\", '$fileIn1.tsv' using 1:12 with filledcurves smooth bezie title 'assetTransfer' lc \"light-red\",  '$fileIn1.tsv' using 1:10 with filledcurves smooth bezie title 'attributeUpdate' lc \"goldenrod\", '$fileIn1.tsv' using 1:8 with filledcurves smooth bezie title 'holderPolicyUpdate' lc \"dark-yellow\", '$fileIn1.tsv' using 1:6 with filledcurves smooth bezie title 'newAssetCreation' lc \"dark-orange\", '$fileIn1.tsv' using 1:4 with filledcurves smooth bezie title 'newCreatorCreation' lc \"green\"\n"  | gnuplot -persist
-#
-#
-#fileOut="simGraph2Operations"
-#size="600,600"
-#echo -e "set output \"$fileOut.$outf\"\n\
-#$same\n\
-#set ylabel 'gas'\n\
-#set title 'Boom and bust'\n\
-#set yrange [0:105000000]\n\
-#set terminal $outff $font size $size\n\
-#plot   '$fileIn2.tsv' using 1:2 with lines title 'Total' smooth bezier lw 3 lc \"medium-blue\", '$fileIn2.tsv' using 1:12 with filledcurves smooth bezie title 'assetTransfer' lc \"light-red\",  '$fileIn2.tsv' using 1:10 with filledcurves smooth bezie title 'attributeUpdate' lc \"goldenrod\", '$fileIn2.tsv' using 1:8 with filledcurves smooth bezie title 'holderPolicyUpdate' lc \"dark-yellow\", '$fileIn2.tsv' using 1:6 with filledcurves smooth bezie title 'newAssetCreation' lc \"dark-orange\", '$fileIn2.tsv' using 1:4 with filledcurves smooth bezie title 'newCreatorCreation' lc \"green\"\n"  | gnuplot -persist
-#
-#
-#
-#
-#
+#plot   '$fileIn1.tsv' using 1:2 with lines title 'Total' smooth bezier lw 3 lc \"medium-blue\", '$fileIn1.tsv' using 1:12 with filledcurves smooth bezie title 'transferFrom' lc \"light-red\",  '$fileIn1.tsv' using 1:10 with filledcurves smooth bezie title 'setColor' lc \"goldenrod\", '$fileIn1.tsv' using 1:8 with filledcurves smooth bezie title 'hPolicyUpdate' lc \"dark-yellow\", '$fileIn1.tsv' using 1:6 with filledcurves smooth bezie title 'newAsset' lc \"dark-orange\", '$fileIn1.tsv' using 1:4 with filledcurves smooth bezie title 'newCreator' lc \"green\"\n"  | gnuplot -persist
+
+
+fileOut="simGraph1Operations"
+size="600,600"
+echo -e "set output \"$fileOut.$outf\"\n\
+$same\n\
+set ylabel 'gas'\n\
+set title 'Single peak'\n\
+set yrange [0:90000000]\n\
+set terminal $outff $font size $size\n\
+plot   '$fileIn1.tsv' using 1:2 with lines title 'Total' smooth bezier lw 3 lc \"medium-blue\",
+'$fileIn1.tsv' using 1:12 with filledcurves smooth bezie title 'assetTransfer' lc \"light-red\",
+ '$fileIn1.tsv' using 1:10 with filledcurves smooth bezie title 'attributeUpdate' lc \"goldenrod\",
+ '$fileIn1.tsv' using 1:8 with filledcurves smooth bezie title 'holderPolicyUpdate' lc \"dark-yellow\",
+ '$fileIn1.tsv' using 1:6 with filledcurves smooth bezie title 'newAssetCreation' lc \"dark-orange\",
+ '$fileIn1.tsv' using 1:4 with filledcurves smooth bezie title 'newCreatorCreation' lc \"green\"\n"  | gnuplot -persist
+
+
+fileOut="simGraph2Operations"
+size="600,600"
+echo -e "set output \"$fileOut.$outf\"\n\
+$same\n\
+set ylabel 'gas'\n\
+set title 'Boom and bust'\n\
+set yrange [0:105000000]\n\
+set terminal $outff $font size $size\n\
+plot   '$fileIn2.tsv' using 1:2 with lines title 'Total' smooth bezier lw 3 lc \"medium-blue\", '$fileIn2.tsv' using 1:12 with filledcurves smooth bezie title 'assetTransfer' lc \"light-red\",  '$fileIn2.tsv' using 1:10 with filledcurves smooth bezie title 'attributeUpdate' lc \"goldenrod\", '$fileIn2.tsv' using 1:8 with filledcurves smooth bezie title 'holderPolicyUpdate' lc \"dark-yellow\", '$fileIn2.tsv' using 1:6 with filledcurves smooth bezie title 'newAssetCreation' lc \"dark-orange\", '$fileIn2.tsv' using 1:4 with filledcurves smooth bezie title 'newCreatorCreation' lc \"green\"\n"  | gnuplot -persist
+
+
+
+
+
 
 
 size="600,600"
